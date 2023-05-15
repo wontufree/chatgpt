1efccc9e97d9a9a00e2896b2801ad77b910a2955
wontufree <wontufree.chan@gmail.com>
2023-05-15 16:40:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,29 +13,29 @@ available_setting = {
     # openai api配置
     "open_ai_api_key": "",  # openai api key
     # openai apibase，当use_azure_chatgpt为true时，需要设置对应的api base
-    "open_ai_api_base": "https://api.openai.com/v1",
+    "open_ai_api_base": "https://ai-dietitian-lvshou.openai.azure.com/",
     "proxy": "",  # openai使用的代理
     # chatgpt模型， 当use_azure_chatgpt为true时，其名称为Azure上model deployment名称
-    "model": "gpt-3.5-turbo",
-    "use_azure_chatgpt": False,  # 是否使用azure的chatgpt
-    "azure_deployment_id": "",  # azure 模型部署名称
+    "model": "text-davinci-003",
+    "use_azure_chatgpt": True,  # 是否使用azure的chatgpt
+    "azure_deployment_id": "AI-Dietitian",  # azure 模型部署名称
     # Bot触发配置
     "single_chat_prefix": ["bot", "@bot"],  # 私聊时文本需要包含该前缀才能触发机器人回复
     "single_chat_reply_prefix": "[bot] ",  # 私聊时自动回复的前缀，用于区分真人
     "group_chat_prefix": ["@bot"],  # 群聊时包含该前缀则会触发机器人回复
     "group_chat_reply_prefix": "",  # 群聊时自动回复的前缀
-    "group_chat_keyword": [],  # 群聊时包含该关键词则会触发机器人回复
+    "group_chat_keyword": ["营养师"],  # 群聊时包含该关键词则会触发机器人回复
     "group_at_off": False,  # 是否关闭群聊时@bot的触发
-    "group_name_white_list": ["ChatGPT测试群", "ChatGPT测试群2"],  # 开启自动回复的群名称列表
-    "group_name_keyword_white_list": [],  # 开启自动回复的群名称关键词列表
-    "group_chat_in_one_session": ["ChatGPT测试群"],  # 支持会话上下文共享的群名称
+    "group_name_white_list": ["chatPPP", "chatttt"],  # 开启自动回复的群名称列表
+    "group_name_keyword_white_list": ["chat"],  # 开启自动回复的群名称关键词列表
+    "group_chat_in_one_session": ["chatPPP", "chatttt"],  # 支持会话上下文共享的群名称
     "trigger_by_self": False,  # 是否允许机器人触发
     "image_create_prefix": ["画", "看", "找"],  # 开启图片回复的前缀
     "concurrency_in_session": 1,  # 同一会话最多有多少条消息在处理中，大于1可能乱序
     "image_create_size": "256x256",  # 图片大小,可选有 256x256, 512x512, 1024x1024
     # chatgpt会话参数
     "expires_in_seconds": 3600,  # 无操作会话的过期时间
-    "character_desc": "你是ChatGPT, 一个由OpenAI训练的大型语言模型, 你旨在回答并解决人们的任何问题，并且可以使用多种语言与人交流。",  # 人格描述
+    "character_desc": "你是一个营养师，只会回答人们营养师相关的问题，每天会给一个食谱，食谱要包含早午晚三餐，三餐热量比例分别为3:4:3",  # 人格描述
     "conversation_max_tokens": 1000,  # 支持上下文记忆的最多字符数
     # chatgpt限流配置
     "rate_limit_chatgpt": 20,  # chatgpt的调用频率限制
